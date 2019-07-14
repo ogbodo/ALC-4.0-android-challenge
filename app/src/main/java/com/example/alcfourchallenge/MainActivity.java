c94af946e1059f35432b5e138d412d3e34227da5
Izuking <matthiasogbodo@gmail.com>
2019-07-14 12:38:10 +0100

    Adds method declarations for activity B and C

--- app/src/main/java/com/example/alcfourchallenge/MainActivity.java
+++ app/src/main/java/com/example/alcfourchallenge/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.alcfourchallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button  aboutALC = findViewById(R.id.about_alc);
+        Button  aboutMe = findViewById(R.id.about_me);
+
+
+
+    }
+
+    public void startActivityC(View view) {
+    }
+
+    public void startActivityB(View view) {
     }
 }
