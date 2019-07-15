27e8dae2e12a09b4e0a389257868904ba6abca76
Izuking <matthiasogbodo@gmail.com>
2019-07-15 08:11:54 +0100

    fixed bugs

--- app/src/main/java/com/example/alcfourchallenge/ActivityB.java
+++ app/src/main/java/com/example/alcfourchallenge/ActivityB.java
@@ -1,6 +1,7 @@
 package com.example.alcfourchallenge;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,9 @@ public class ActivityB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_alc);
-        WebView webView = findViewById(R.id.alc_url);
+        WebView webView = findViewById(R.id.webview);
         webView.loadUrl("https://andela.com/alc/");
+
 
     }
 
